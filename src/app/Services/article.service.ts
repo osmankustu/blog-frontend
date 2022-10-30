01852e3753fd1cd8c5c543513a96821c651a7cbb
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2022-10-30 18:51:54 +0300

    makaleye gitme ve yorum özelliiği eklendi

--- src/app/Services/article.service.ts
+++ src/app/Services/article.service.ts
@@ -4,18 +4,19 @@ import { Observable } from 'rxjs';
 import { Article } from '../Models/article';
 import { ListResponseModel } from '../Models/listResponseModel';
 import { ResponseModel } from '../Models/responseModel';
+import { SingleResponseModel } from '../Models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  apiUrl = 'https://localhost:44335/api/';
+  apiUrl = 'http://osmankustu.cf/api/';
 
   constructor(private httpClient: HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Article>> {
-    let newPath = this.apiUrl + "Articles/getall"
+    let newPath = this.apiUrl + "Articles/Getall"
     return this.httpClient.get<ListResponseModel<Article>>(newPath);
   }
 
@@ -29,5 +30,9 @@ export class ArticleService {
 
   add(article:Article):Observable<ResponseModel>{
     return this.httpClient.post<ResponseModel>(this.apiUrl+"Articles/add",article)
+  }
+  getArticleByArticleId(articleId:number):Observable<SingleResponseModel<Article>>{
+    let newPath = this.apiUrl + "Articles/GetById?CategoryId="+articleId  
+    return this.httpClient.get<SingleResponseModel<Article>>(newPath);
   }
 }
